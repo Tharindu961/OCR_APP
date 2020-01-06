9634bba86b8a28061bad715c446d51cda4b7e176
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-01-06 16:06:02 +0530

    updated
    updated user login backend part

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 // mongoose.connect('mongodb://test:' + process.env.MONGO_ATLAS_PW + '@cluster0-shard-00-00-0c64g.mongodb.net:27017,cluster0-shard-00-01-0c64g.mongodb.net:27017,cluster0-shard-00-02-0c64g.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
 mongoose.connect('mongodb+srv://test:'+ process.env.MONGO_ATLAS_PW +'@cluster0-0c64g.mongodb.net/test?retryWrites=true&w=majority',
@@ -25,6 +26,7 @@ app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
@@ -38,6 +40,7 @@ app.use((req, res, next) => {
 //Routes should handle request
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) =>{
     const error = new Error('Not Found');
