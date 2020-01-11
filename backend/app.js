12a14a1114057c8f9007f4702dde26ae065b1571
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-01-11 20:54:40 +0530

    backend updated
    updated the routes

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
-const userRoutes = require('./api/routes/user');
+
+const userRoutes = require('./api/routes/users');
 
 //mongoose.connect('mongodb://test:' + process.env.MONGO_ATLAS_PW + '@cluster0-shard-00-00-0c64g.mongodb.net:27017,cluster0-shard-00-01-0c64g.mongodb.net:27017,cluster0-shard-00-02-0c64g.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
 // mongoose.connect('mongodb+srv://test:' + process.env.MONGO_ATLAS_PW + '@cluster0-0c64g.mongodb.net/test?retryWrites=true&w=majority',
@@ -40,9 +39,6 @@ app.use((req, res, next) => {
     next(); 
 });
 
-//Routes should handle request
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
 app.use('/user', userRoutes);
 
 app.use((req, res, next) =>{
